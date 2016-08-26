2f02511a03941f3b5aee5f07bb14990c76ac095c
Leviraptor <niamhphotography@outlook.com>
2016-08-26 10:10:12 +0100

    addedwalls robot mountian tarrain

---
+++ Assets/person.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu]
+public class person : ScriptableObject {
+public int age;
+public string job;
+public string pet; 
+public int numberOfAxlotls; 
+public string gunType; 
+}
